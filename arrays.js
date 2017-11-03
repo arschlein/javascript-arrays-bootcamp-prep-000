d2a533e365b05c5602d6a383e1c8efdeb260b266
Monika <arschlein@seznam.cz>
2017-11-03 14:36:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,11 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element)
-  return array
-}
-function destructivelyAddElementToBeginningOfArray(array, element){
   var newArray = [element, ...array]
   return newArray
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
 }
 
 function addElementToEndOfArray(array, element){
